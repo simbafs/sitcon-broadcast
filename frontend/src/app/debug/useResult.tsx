2a4648e457fa188aa09e6917728a648c1e96e449
SimbaFs <simba.fs@gmail.com>
2025-03-04 22:35:52 +0800

    format

--- frontend/src/app/debug/useResult.tsx
+++ frontend/src/app/debug/useResult.tsx
@@ -4,20 +4,20 @@ export const input = 'm-2 p-2 bg-blue-200'
 export const output = 'm-2 p-2 bg-green-200'
 
 export function useResult() {
-    const [result, setResult] = useState('')
+	const [result, setResult] = useState('')
 
-    function Result() {
-        return <pre className={output}>{result}</pre>
-    }
+	function Result() {
+		return <pre className={output}>{result}</pre>
+	}
 
-    function set(r: any) {
-        if (r instanceof Error) setResult('error: ' + r.message)
-        else setResult(JSON.stringify(r, null, 2))
-    }
+	function set(r: any) {
+		if (r instanceof Error) setResult('error: ' + r.message)
+		else setResult(JSON.stringify(r, null, 2))
+	}
 
-    function clear() {
-        setResult('')
-    }
+	function clear() {
+		setResult('')
+	}
 
-    return { Result, set, clear }
+	return { Result, set, clear }
 }

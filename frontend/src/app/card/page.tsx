e51394d6d9edf049424d24dd7eceb402ef13f1c0
SimbaFs <simba.fs@gmail.com>
2025-03-05 17:13:17 +0800

    slido, slides and hackmd

--- frontend/src/app/card/page.tsx
+++ frontend/src/app/card/page.tsx
@@ -1,13 +1,14 @@
 'use client'
 
-import Image from 'next/image'
-import { twMerge } from 'tailwind-merge'
-import x from './x.svg'
 import { formatTime } from '@/utils/formatTime'
 import { useSSE } from '@/hooks/useSSE'
 import { Suspense, useEffect, useState } from 'react'
 import { parseAsString, useQueryState } from 'nuqs'
 import { ensureSession, GetCurrentSession, GetSessionByID, Session, ZeroSession } from '@/sdk/sdk'
+import QRCode from 'react-qr-code'
+import Image from 'next/image'
+
+import slido from './slido.png'
 
 function useCard() {
 	const [room] = useQueryState('room', parseAsString.withDefault('R0'))
@@ -34,23 +35,11 @@ function useCard() {
 	return [card, error] as const
 }
 
-function Card() {
-	const [card, error] = useCard()
-
+function Card({ card }: { card: Session }) {
 	const speakers = card.speakers.join('、')
 
-	// TODO: should I show the error?
-	// if (error) {
-	// 	return (
-	// 		<div className="flex grow flex-col px-[6vw] py-[2vw]">
-	// 			<h1 className="text-[5vw] text-[#9f3b24]">Error</h1>
-	// 			<pre>{error.message}</pre>
-	// 		</div>
-	// 	)
-	// }
-
 	return (
-		<div className="flex grow flex-col justify-between p-2 font-card">
+		<div className="font-card flex grow flex-col justify-between p-2">
 			<div className="flex items-center justify-between">
 				<p className="text-3xl text-[#2540a7]">{card.type}</p>
 				<p className="text-2xl text-[#8144b5]">
@@ -63,15 +52,70 @@ function Card() {
 	)
 }
 
-export default function Page() {
+function Slido({ card }: { card: Session }) {
+	if (!card.slido) return <p>No Slido</p>
+	return (
+		<div className="grid grid-cols-[40px_1fr] place-content-center gap-2 bg-[#9698c8] h-full w-full p-2">
+			<Image src={slido} alt="slido" width={40} height={40} />
+			<div className="flex items-center">
+				<p className="text-white text-3xl font-card">Slido</p>
+			</div>
+			<p />
+			<p className="text-[#37ca34] text-3xl font-card">#{card.slido}</p>
+		</div>
+	)
+}
+
+function Slides({ card }: { card: Session }) {
+	if (!card.slide) return <p>No Image</p>
+	return <QRCode value={card.slide} />
+}
+
+function Hackmd({ card }: { card: Session }) {
+	if (!card.hackmd) return <p>No Hackmd</p>
+	return <QRCode value={card.hackmd} />
+}
+
+function SessionCard() {
+	const [card, _] = useCard()
+
 	return (
 		<>
-			<div className="flex min-h-[250px] w-[400px] flex-col bg-[#fdfdfd]">
-				<Suspense>
-					<Card />
-				</Suspense>
+			<div className="w-fit border-4 border-green-500">
+				<div className="flex min-h-[250px] w-[400px] flex-col bg-[#fefefe]">
+					<Card card={card} />
+				</div>
 			</div>
 			<p className="mt-2">400px x 250px</p>
+
+			<div className=" w-fit border-4 border-green-500 mt-2">
+				<div className="h-[100px] w-[350px]">
+					<Slido card={card} />
+				</div>
+			</div>
+			<p className="mt-2">350px x 100px</p>
+			<div className="mt-2 grid w-fit grid-cols-2 gap-2">
+				<div className=" w-fit border-4 border-green-500">
+					<div className="grid h-[256px] w-[256px] place-content-center">
+						<Slides card={card} />
+					</div>
+				</div>
+				<div className="w-fit border-4 border-green-500">
+					<div className="grid h-[256px] w-[256px] place-content-center">
+						<Hackmd card={card} />
+					</div>
+				</div>
+				<p>256px x 256px</p>
+				<p>256px x 256px</p>
+			</div>
 		</>
+	)
+}
+
+export default function Page() {
+	return (
+		<Suspense>
+			<SessionCard />
+		</Suspense>
 	)
 }

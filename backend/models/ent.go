5f9f0e7647323b79b72c923c5683203bf9c54595
SimbaFs <simba.fs@gmail.com>
2025-04-18 20:20:14 +0800

    fix bug when the directory of database is not exist

--- backend/models/ent.go
+++ backend/models/ent.go
@@ -2,6 +2,9 @@ package models
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"path/filepath"
 
 	"backend/ent"
 
@@ -11,6 +14,9 @@ import (
 var Client *ent.Client
 
 func InitDB(db string) error {
+	if err := os.MkdirAll(filepath.Dir(db), 0o755); err != nil {
+		return fmt.Errorf("failed to create directory for db: %w", err)
+	}
 	var err error
 	Client, err = ent.Open("sqlite3", "file:"+db+"?cache=shared&_fk=1")
 	if err != nil {

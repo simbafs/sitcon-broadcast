470d098cfc90e01a8ab11580004d049e3838306b
SimbaFs <simba.fs@gmail.com>
2025-04-23 00:06:50 +0800

    fix frontend build error

--- frontend/src/app/card/all/page.tsx
+++ frontend/src/app/card/all/page.tsx
@@ -2,6 +2,7 @@
 
 // TODO: 顯示成時間表
 import { Card } from '@/components/card'
+import { Loading } from '@/components/loading'
 import { useSessions } from '@/hooks/useSessions'
 import { parseAsString, useQueryState } from 'nuqs'
 import { Suspense } from 'react'
@@ -14,9 +15,7 @@ function All() {
 		<div className="m-6 flex flex-col items-center">
 			<div className=" max-w-2xl">
 				<h1 className="m-10 text-center text-5xl font-bold">{room}</h1>
-				{sessions.map(session => (
-					<Card key={session.session_id} session={session} />
-				))}
+				{sessions ? sessions.map(session => <Card key={session.session_id} session={session} />) : <Loading />}
 			</div>
 		</div>
 	)
